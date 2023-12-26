d61d1ddba8b4c365d648764700cea4e2ac1098f0
Withered Flowers <withered-flowers@users.noreply.github.com>
2023-12-26 12:36:32 +0700

    feat: finalize SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import react from "@astrojs/react";
+import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
-
-import svelte from "@astrojs/svelte";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), vue(), svelte()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
